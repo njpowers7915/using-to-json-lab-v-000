7462e903785887f1b98dadb0ad2d2877cfff5dbd
Nicholas Powers <njpowers7915@gmail.com>
2018-08-14 14:27:47 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,7 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
-  def create
+  def create 
     Product.create(product_params)
     redirect_to products_path
   end
@@ -25,7 +25,7 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     respond_to do |format|
-      format.html {render: show}
+      format.html {render :show}
       format.json {render json: @product.to_json(only: [:id, :name, :description, :inventory, :price])}
   end
 
