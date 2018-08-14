95083b6fd51dcfa7b6d91dd0172f2ee91af028d2
Nicholas Powers <njpowers7915@gmail.com>
2018-08-14 14:27:51 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,7 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
-  def create 
+  def create
     Product.create(product_params)
     redirect_to products_path
   end
